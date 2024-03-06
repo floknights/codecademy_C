e9a5c2271c1fb5038c52a8d0cde44106d668f4c0
Flo <123563998+floknights@users.noreply.github.com>
2024-03-06 12:15:44 +0000

    Update leapYear.c
    Using logical operators to refactor original code

--- leapYear.c
+++ leapYear.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 bool leapYear(int year) {
-  if (year % 4 != 0) {
-    return false;
-} else if (year % 100 != 0) {
-    return true;
-} else if (year % 400 != 0) {
-    return false;
-} else {
-    return true;
-  }
+  return (year % 4 == 0 && (year % 100 || year % 400 == 0));
 }
 
 int main() {
